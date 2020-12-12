aa89decfd2aa670c9cb7f31b63d17476aa0b012e
Ameen Sayegh <aameen951@gmail.com>
2020-12-12 09:27:00 +0300

    Day12: improved the code.

--- day12.js
+++ day12.js
@@ -4,59 +4,81 @@ let part_one_solution = null;
 let part_two_solution = null;
 
 let raw_data = fs.readFileSync("data/day12.txt", {encoding:"utf8"}).trim();
-// raw_data = `F10
-// N3
-// F7
-// R90
-// F11`;
 
 /// PROCESSING
-const instructions = raw_data.split("\n").map(x => x.match(/^(.)(\d+)$/).slice(1));
 
-/// UTILS
+// represent directions as numbers to allow using add/sub to turn right/left.
+const N=0, E=1, S=2, W=3;
+// parse_map converts characters to symbols.
+const parse_map = {N:N, E:E, S:S, W:W, L:'L', R:'R', F:'F'};
+
+// each line has one instruction.
+const instructions = raw_data.split("\n").map(x => {
+  // syntax: <char> <number>
+  const m = x.match(/^(.)(\d+)$/);
+  return {i:parse_map[m[1]], v: +m[2]};
+});
+
 /// PART ONE
 
 {
-  let dir = 'E';
+  // current direction of the ship.
+  let dir = E;
+  // current coordinates of the ship (positive is N or E, negative is S or W).
   let x = 0, y = 0;
-  const right = {E:'S', S:'W', W:'N', N:'E'};
-  const left = {E:'N', S:'E', W:'S', N:'W'};
-  for(let i=0; i<instructions.length; i++)
+  
+  for(let idx=0; idx<instructions.length; idx++)
   {
-    const inst = instructions[i];
-    let c = inst[0];
+    let {i, v} = instructions[idx];
 
-    if(c == 'L')for(let j=0; j<+inst[1]; j+=90)dir = left[dir];
-    else if(c == 'R')for(let j=0; j<+inst[1]; j+=90)dir = right[dir];
-    else
+    // if Forward, then use the current direction.
+    if(i == 'F')i = dir;
+
+    switch(i)
     {
-      if(c == 'F')c = dir;
-          if(c == 'N')y += +inst[1];
-      else if(c == 'S')y -= +inst[1];
-      else if(c == 'E')x += +inst[1];
-      else if(c == 'W')x -= +inst[1];
+      // update current coordinate according to the direction using specified value.
+      case N: y += v; break;
+      case S: y -= v; break;
+      case E: x += v; break;
+      case W: x -= v; break;
+
+      // add/sub to current direction the number of quarters to turn.
+      // add for right, sub for left.
+      // we use mod to wrap W+1 to N and N-1 to W.
+      case 'L': dir = (dir - v/90 + 4) % 4; break;
+      case 'R': dir = (dir + v/90    ) % 4; break;
     }
   }
+  // The Manhattan distance is the solution.
   part_one_solution = Math.abs(x)+Math.abs(y);
 }
 
 /// PART TWO
 {
-  let wx = 10, wy = 1;
-  let x = 0, y = 0;
-  for(let i=0; i<instructions.length; i++)
-  {
-    const inst = instructions[i];
-    let c = inst[0];
+  // x,y are the coordinates of the ship. 
+  // wx,wy are the coordinates of the waypoint.
+  let x = 0, y = 0, wx = 10, wy = 1;
 
-    if(c == 'L')for(let j=0; j<+inst[1]; j+=90){ [wx, wy] = [-wy, wx]; }
-    else if(c == 'R')for(let j=0; j<+inst[1]; j+=90){ [wx, wy] = [wy, -wx]; }
-    else if(c == 'F'){x += wx*(+inst[1]); y += wy*(+inst[1]);}
-    else if(c == 'N')wy += +inst[1];
-    else if(c == 'S')wy -= +inst[1];
-    else if(c == 'E')wx += +inst[1];
-    else if(c == 'W')wx -= +inst[1];
+  for(let idx=0; idx<instructions.length; idx++)
+  {
+    let {i, v} = instructions[idx];
+    switch(i)
+    {
+      // update the waypoint coordinate according to the direction using value.
+      case N: wy += v; break;
+      case S: wy -= v; break;
+      case E: wx += v; break;
+      case W: wx -= v; break;
+
+      // move the ship to the waypoint `v` times.
+      case 'F': x += wx*v; y += wy*v; break;
+      
+      // for each turning quarter, rotate the waypoint coordinates.
+      case 'L': for(let j=0; j<v; j+=90) [wx, wy] = [-wy,  wx]; break;
+      case 'R': for(let j=0; j<v; j+=90) [wx, wy] = [ wy, -wx]; break;
+    }
   }
+  // The Manhattan distance is the solution.
   part_two_solution = Math.abs(x)+Math.abs(y);
 }
 
